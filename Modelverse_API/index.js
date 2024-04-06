bc719be951e07db02f684e86cf72ed40b612c84b
timsdesigns <tims.designs@yahoo.com>
2024-04-06 08:01:17 +0200

    adding todos

--- Modelverse_API/index.js
+++ Modelverse_API/index.js
@@ -1,3 +1,20 @@
 (() => {
-  
+  // TODO: Import modules
+  // TODO: Assign functions
+  // TODO: Swagger Setup
+  // TODO: Placeholders for DTO as JSON strings
+  // TODO: Middleware-Chain: log, auth user, parse json, serve static, route app, handle err, listen req
+  // TODO: Declaration: function(req, res, next) // Call/Invocation: app.use(function),
+  // TODO: Routing and Handling:
+  // TODO: READ   Get a list of all models
+  // TODO: READ   Get data about a single model by title
+  // TODO:  (description, category, property, model URL, whether it’s featured or not)
+  // TODO: READ   Get data about a category by name (description)
+  // TODO: READ   Get data about a director by property (vertex count, materials, size)
+  // TODO: CREATE Register a new user
+  // TODO: UPDATE Update user info (username)
+  // TODO: CREATE Add model to favorites (showing only a text that a model has been added)
+  // TODO: DELETE Remove model from favorites (showing only a text that a model has been removed)
+  // TODO: DELETE Deregister User (showing only a text that a user email has been removed)
+  // TODO: Express HTTP implementation: app.METHOD(PATH, HANDLER(responseLogic))
 })()
